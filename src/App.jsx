35eaef64274327bb539451a8937b6d503949f128
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-15 02:31:08 +0530

    Add done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Add from './components/Add'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
       
+        <Routes>
+
+          <Route path='/' element={<Home/>} />
+          <Route path='/add' element={<Add/>} />
+
+
+        </Routes>
+      
+      </BrowserRouter>
     </>
   )
 }
